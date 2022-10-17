8d29d56b9aad1c8b94ce4f89b1f03630c5c57037
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2022-10-17 12:10:19 +0530

    Update frequency.java

--- frequency.java
+++ frequency.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
- class frequency     
-{    
-     public static void main(String[] args) {   
-        Scanner s = new Scanner(System.in);
-          System.out.println("Enter the string");
-         n = s.nextInt();
-        int[] freq = new int[str.length()];    
-        int i, j;       
-        char string[] = str.toCharArray();    
-            
-        for(i = 0; i <str.length(); i++) {    
-            freq[i] = 1;    
-            for(j = i+1; j <str.length(); j++) {    
-                if(string[i] == string[j]) {    
-                    freq[i]++;        
-                    string[j] = '0';    
-                }    
-            }    
-        }        
-        System.out.println("Characters and their corresponding frequencies");    
-        for(i = 0; i <freq.length; i++) {    
-            if(string[i] != ' ' && string[i] != '0')    
-                System.out.println(string[i] + "-" + freq[i]);    
-        }    
-    }    
-}
+class frequency   
+{  
+     public static void main(String[] args) {  
+          Scanner k = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str= k.nextLine();
+        int[] freq = new int[str.length()];  
+        int i, j;  
+          
+        //Converts given string into character array  
+        char string[] = str.toCharArray();  
+          
+        for(i = 0; i <str.length(); i++) {  
+            freq[i] = 1;  
+            for(j = i+1; j <str.length(); j++) {  
+                if(string[i] == string[j]) {  
+                    freq[i]++;  
+                      
+                    //Set string[j] to 0 to avoid printing visited character  
+                    string[j] = '0';  
+                }  
+            }  
+        }  
+          
+        //Displays the each character and their corresponding frequency  
+        System.out.println("Characters and their corresponding frequencies");  
+        for(i = 0; i <freq.length; i++) {  
+            if(string[i] != ' ' && string[i] != '0')  
+                System.out.println(string[i] + "-" + freq[i]);  
+        }  
+    }  
+}  

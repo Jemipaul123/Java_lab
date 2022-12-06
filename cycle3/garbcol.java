27289c08008d8ef2431e71185e6cde2dce474222
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2022-12-06 22:39:20 +0530

    Update garbcol.java

--- cycle3/garbcol.java
+++ cycle3/garbcol.java
@@ -1,48 +1,34 @@
-class garbcoll
+import java.util.Scanner;
+class garbage
 {
-    
-    public static void main(String arg[])
-    {
-        Runtime gfg = Runtime.getRuntime();
-        long memory1, memory2;
-        Integer integer[] = new Integer[1000];
- 
-        // checking the total memory
-        System.out.println("Total memory is: "
-                           + gfg.totalMemory());
- 
-        // checking free memory
-        memory1 = gfg.freeMemory();
-        System.out.println("Initial free memory: "
-                                      + memory1);
- 
-        // calling the garbage collector on demand
-        gfg.gc();
- 
-        memory1 = gfg.freeMemory();
- 
-        System.out.println("Free memory after garbage "
-                           + "collection: " + memory1);
- 
-        // allocating integers
-        for (int i = 0; i < 1000; i++)
-            integer[i] = new Integer(i);
- 
-        memory2 = gfg.freeMemory();
-        System.out.println("Free memory after allocation: "
-                           + memory2);
- 
-        System.out.println("Memory used by allocation: " +
-                                    (memory1 - memory2));
- 
-        // discard integers
-        for (int i = 0; i < 1000; i++)
-            integer[i] = null;
- 
-        gfg.gc();
- 
-        memory2 = gfg.freeMemory();
-        System.out.println("Free memory after  "
-            + "collecting discarded Integers: " + memory2);
-    }
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  Runtime r = Runtime.getRuntime();
+  long m1,m2,m3;
+  Integer someints[]=new Integer[100];
+  System.out.println("Total memory is:"+r.totalMemory());
+  m1=r.freeMemory();
+  System.out.println("Initial free memory:"+m1);
+  r.gc();
+  m1=r.freeMemory();
+  System.out.println("Free memory after garbage collection:"+m1);
+  
+  System.out.println("Enter 5 integer values: \n");
+  for(int i=0;i<5;i++)
+		someints[i]=sc.nextInt();
+		sc.nextLine();
+
+  m2=r.freeMemory();
+  System.out.println("Free memory after input:"+m2);
+	
+  for(int i=0;i<10;i++)
+	 	someints[i]=null;
+
+  m2=r.freeMemory();
+  System.out.println("Free memory after input is made null:"+m2);
+  r.gc();
+  m3=r.freeMemory();
+  System.out.println("Free memory after garbage collection:"+m3);
+ }
 }
